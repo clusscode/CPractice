688e1a894b3077024e8fe56c1e600a43f3f6e2a0
clusscode <77377776+clusscode@users.noreply.github.com>
2021-01-18 17:39:30 +0800

    2021.1.18——第五例
    输入年份日期，判断该天为这一年的第几天

--- test.c
+++ test.c
@@ -1,26 +1,37 @@
-//C语言100 - 第1例
-//题目：有1、2、3、4个数字，能组成多少个互不相同且无重复数字的三位数？都是多少？
-//程序分析：可填在百位、十位、个位的数字都是1、2、3、4。组成所有的排列后再去掉不满足条件的排列。
-#define _CRT_SECURE_NO_WARNINGS
-#include<stdio.h>
-#include<stdlib.h>
+//题目：用户输入一个日期，打印出这个日期是这年的第几天
+//程序思想：先用switch语句计算出平常年份的月的天数，然后在控制是否为闰年在确定是否加1
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
 int main()
 {
-	int ge, shi, bai;//定义百位，十位以及个位
-	int sum = 0;//定义sum，用来记录有多少个满足条件的三位数
-	//进行三重循环，组合出所有的三位数
-	for (ge = 1; ge <= 4; ge++) {
-		for (shi = 1; shi <= 4; shi++) {
-			for (bai = 1; bai <= 4; bai++) {
-				//if语句来选出满足条件的三位数
-				if (ge != shi && ge != bai && shi != bai) {
-					sum++;
-
-					printf("%d%d%d\n", bai, shi, ge);
-				}
-			}
-		}
-	}				
-	printf("1,2,3,4能组成%d个互不相同且无重复的三位数\n", sum);
+	int month, year, day, sum;
+	printf("请输入日期，格式为：2021.1.18：");
+	scanf("%d.%d.%d", &year, &month, &day);
+	switch (month)
+	{
+	case 1: sum = 0; break;
+	case 2: sum = 31; break;
+	case 3: sum = 59; break;
+	case 4: sum = 90; break;
+	case 5: sum = 120; break;
+	case 6: sum = 151; break;
+	case 7: sum = 181; break;
+	case 8: sum = 212; break;
+	case 9: sum = 243; break;
+	case 10: sum = 273; break;
+	case 11: sum = 304; break;
+	case 12: sum = 334; break;
+	}
+	sum = sum + day;
+	//当为闰年且输入的月份大于二月时要加上一天
+	if (year%400 == 0 && month > 2)
+	{
+		sum++;
+	}
+	else if (year % 4 == 0 && year % 100 != 0 && month > 2) {
+		sum++;
+	}
+	printf("%d.%d.%d是今年的第%d天\n", year, month, day, sum);
 	return 0;
+	
 }
