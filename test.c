4afedbf4a1ae1b0755d795d2e6957f35a65a8b88
clusscode <77377776+clusscode@users.noreply.github.com>
2021-01-21 11:00:22 +0800

    第六例
    使得字符向中间出现式的靠拢，这道题的考点在与怎么使两个字符串数组交换内容

--- test.c
+++ test.c
@@ -1,35 +1,25 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #include<stdio.h>
+#include<string.h>
+#include<windows.h>
+#include<stdlib.h>
 int main()
 {
-	
-	int i = 0;
-	int arr[10] = { 1,2,3,4,5,6,7,9,10,11 };
-	int left = 0, right = sizeof(arr)/sizeof(int);
-	int n = 0, mid = (left + right) / 2;
-	printf("输入你要查找的数:>");
-	scanf("%d", &n);
-	while(left <= right)
+	char arr1[] = { "Welcome to Chaina!!!!" };
+	char arr2[] = { "#####################" };
+	int left = 0;
+	int right = strlen(arr1) - 1;
+
+	while (left <= right)
 	{
-		 if (arr[mid] > n)
-		{
-			 right = mid - 1;
-			 mid = (left + right) / 2;
-		}
-		else if (arr[mid] < n)
-		{
-			left = mid + 1;
-			mid = (left + right) / 2;
-		}
-		else if (arr[mid] == n)
-		{
-			printf("%d所在的位置为%d", n, mid);
-			break;
-		}
+		arr2[left] = arr1[left];
+		arr2[right] = arr1[right];
+		printf("%s\n", arr2);
+		left++;
+		right--;
+		Sleep(1000);
+		system("cls");
 	}
-	if (left > right)
-	{
-		printf("查无此元素\n");
-	}
+	printf("%s\n", arr2);
 	return 0;
 }
