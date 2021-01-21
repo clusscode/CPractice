1e3c895612d094b88f326313d686ec29880c19e5
clusscode <77377776+clusscode@users.noreply.github.com>
2021-01-21 11:03:48 +0800

    2021.1.19——二分法排序（二次上传）
    二分法排序的难点在与怎样理解二分法的具体实施方法，以及left，right怎样改变

---
+++ 第五例.c
@@ -0,0 +1,35 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+int main()
+{
+	
+	int i = 0;
+	int arr[10] = { 1,2,3,4,5,6,7,9,10,11 };
+	int left = 0, right = sizeof(arr)/sizeof(int);//定义最低位和最高位
+	int n = 0, mid = (left + right) / 2;
+	printf("输入你要查找的数:>");
+	scanf("%d", &n);
+	while(left <= right)
+	{
+		 if (arr[mid] > n)
+		{
+			 right = mid - 1;
+			 mid = (left + right) / 2;
+		}
+		else if (arr[mid] < n)
+		{
+			left = mid + 1;
+			mid = (left + right) / 2;
+		}
+		else if (arr[mid] == n)
+		{
+			printf("%d所在的位置为%d", n, mid);
+			break;
+		}
+	}
+	if (left > right)
+	{
+		printf("查无此元素\n");
+	}
+	return 0;
+}
